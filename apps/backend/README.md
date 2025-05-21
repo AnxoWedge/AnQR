51ebba1afd04b52608cad13d7d6c6ae9a4455a6b
Ângelo Cunha <38915822+AnxoWedge@users.noreply.github.com>
2025-05-21 20:51:37 +0100

    Server side base

---
+++ apps/backend/README.md
@@ -0,0 +1,25 @@
+# QR Code Generator 
+
+This is a little experiment of mine in trying to make a new QR Code Generator. This will be used for various events, brands and other usages in order to have a free version qrcode, that let's me do QRcodes without the strings attached that some options online have. 
+
+## Requirements 
+
+This App is being developed with the following tech stack currently: 
+- Python 3.11
+- pip and venv
+- qrcode
+- Pillow
+
+## What is planned
+
+Things planned in adding: 
+- Functions from Pillow to add customization to QRCode 
+- A terminal-based interaction for the app
+- Will have an UI and Front-end for interaction like a webapp 
+- Maybe a creation of an AI for a AI-Based customized QRcode
+
+This will include the following tech for the completion of these current objections: 
+- Flask
+- A React or Nextjs page / Astro
+- Either PyTorch, Tensorflow, or brain.js
+ 
---
+++ apps/backend/app.py
@@ -0,0 +1,66 @@
+from flask import Flask, request, send_file
+from flask_cors import CORS
+import qrcode
+from PIL import Image
+import qrcode.constants
+import os
+
+app = Flask(__name__)
+
+cors = CORS(app)
+
+@app.route('/generate_qr', methods=['POST'])
+def generate_qr():
+    data = request.json
+    print(data)
+    url = data.get('url')
+    logo_link = data.get('logo', None)  # Optional logo link
+
+    # Check if URL is provided
+    if not url:
+        return {"error": "URL is required"}, 400
+
+    # Check if logo exists and open it if provided
+    logo = None
+    if logo_link:
+        try:
+            logo = Image.open(logo_link)
+            # width 
+            basewidth = 150
+
+            # adjust image size 
+            wpercent = (basewidth / float(logo.size[0]))
+            hsize = int((float(logo.size[1]) * float(wpercent)))
+            logo = logo.resize((basewidth, hsize), Image.ANTIALIAS)
+        except FileNotFoundError:
+            logo = None  # Logo is optional, set to None if not found
+
+    # Create QR code
+    Qrcode = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
+    Qrcode.add_data(url)
+    Qrcode.make()
+
+    # Asking for color and adding color
+    QRcolor = '#000'
+    BGcolor = '#fff'
+
+    # Generate the QR code image
+    QRimg = Qrcode.make_image(
+        fill_color=QRcolor,
+        back_color=BGcolor,
+    ).convert('RGB')
+
+    # If logo is provided, paste it on the QR code
+    if logo:
+        pos = ((QRimg.size[0] - logo.size[0]) // 2,
+               (QRimg.size[1] - logo.size[1]) // 2)
+        QRimg.paste(logo, pos)
+
+    # Save the QR code image
+    qr_code_path = 'QRgenerated.png'
+    QRimg.save(qr_code_path)
+
+    return send_file(qr_code_path, mimetype='image/png')
+
+if __name__ == '__main__':
+    app.run(debug=True)
